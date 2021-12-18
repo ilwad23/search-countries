1974d62dddc019fc2f7778420ef7a6ea7bf48051
Ilwad Abukor <ilwadabukor@hotmail.com>
2021-12-18 10:08:44 +0000

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Search Countries</title>
+        <meta
+          name="description"
+          content="app that helps see and search for countries"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
