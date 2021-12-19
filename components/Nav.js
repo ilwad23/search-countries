d39102302cd6be55e0bc596ac4ff481d7b6d6e16
Ilwad Abukor <ilwadabukor@hotmail.com>
2021-12-19 06:21:12 +0000

    finished

---
+++ components/Nav.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { SunIcon, MoonIcon } from "@heroicons/react/outline";
+import { useStateValue } from "../states/StateProvider";
+import NavStyles from "../styles/Nav.module.scss";
+function Nav() {
+  const [{ toggle }, dispatch] = useStateValue();
+  return (
+    <div className={NavStyles["nav"]}>
+      <h1 className={NavStyles["nav__strong"]}>Where in the world?</h1>
+      <div
+        className={NavStyles["nav__toggleContainer"]}
+        onClick={() => dispatch({ type: "toggle" })}
+      >
+        <div className={NavStyles["nav__icon"]}>
+          {toggle ? <MoonIcon /> : <SunIcon />}
+        </div>
+        <p className={NavStyles["nav__light"]}>Dark Mode</p>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
