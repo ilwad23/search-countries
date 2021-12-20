3f212f6de232a5714b1440a720a76c7115ab5d81
Ilwad Abukor <ilwadabukor@hotmail.com>
2021-12-20 07:31:31 +0000

    updated

--- components/Tools/index.js
+++ components/Tools/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { SearchIcon, ChevronDownIcon } from "@heroicons/react/outline";
-// import { useStateValue } from "../states/StateProvider";
+import { useStateValue } from "../../states/StateProvider";
 import ToolsStyles from "../../styles/components/Tools.module.scss";
 
 function Tools() {
+  const [{ inputValue, filterValue }, dispatch] = useStateValue();
   return (
     <div className={ToolsStyles.tools}>
       <inputContainer className={ToolsStyles.input}>
@@ -11,6 +12,10 @@ function Tools() {
           type="text"
           className={ToolsStyles.input__field}
           placeholder="Search for a country"
+          value={inputValue}
+          onChange={(e) =>
+            dispatch({ type: "setInputValue", val: e.target.value })
+          }
         />
         <iconContainer className={ToolsStyles.input__icon}>
           <SearchIcon />
@@ -18,17 +23,48 @@ function Tools() {
       </inputContainer>
       <filter className={ToolsStyles.filter}>
         <selector className={ToolsStyles.selector}>
-          <p className={ToolsStyles.selector__para}>Filter by Region</p>
+          <p className={ToolsStyles.selector__para}>
+            {filterValue ? filterValue : "Filter by Region"}
+          </p>
           <iconContainer className={ToolsStyles.selector__icon}>
             <ChevronDownIcon />
           </iconContainer>
         </selector>
         <ul className={ToolsStyles.list}>
-          <li className={ToolsStyles.list__para}>Africa</li>
-          <li className={ToolsStyles.list__para}>America</li>
-          <li className={ToolsStyles.list__para}>Asia</li>
-          <li className={ToolsStyles.list__para}>Europe</li>
-          <li className={ToolsStyles.list__para}>Oceania</li>
+          <li
+            className={ToolsStyles.list__para}
+            onClick={(e) => dispatch({ type: "setFilterValue", val: "Africa" })}
+          >
+            Africa
+          </li>
+          <li
+            className={ToolsStyles.list__para}
+            onClick={(e) =>
+              dispatch({ type: "setFilterValue", val: "America" })
+            }
+          >
+            America
+          </li>
+          <li
+            className={ToolsStyles.list__para}
+            onClick={(e) => dispatch({ type: "setFilterValue", val: "Asia" })}
+          >
+            Asia
+          </li>
+          <li
+            className={ToolsStyles.list__para}
+            onClick={(e) => dispatch({ type: "setFilterValue", val: "Europe" })}
+          >
+            Europe
+          </li>
+          <li
+            className={ToolsStyles.list__para}
+            onClick={(e) =>
+              dispatch({ type: "setFilterValue", val: "Oceania" })
+            }
+          >
+            Oceania
+          </li>
         </ul>
       </filter>
     </div>
--- states/reducer.js
+++ states/reducer.js
@@ -1,9 +1,22 @@
 export const initialState = {
   toggle: true,
+  inputValue: "",
+  filterValue: "",
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
+    case "setFilterValue":
+      console.log(action.val);
+    return {
+          ...state,
+          filterValue: action.val,
+        };
+    case "setInputValue":
+    return {
+          ...state,
+          inputValue: action.val,
+        };
     case "toggle":
         return {
         ...state,
